ae193ed6d714f03893e1c142f4084f091cd83e8d
miku86 <miku86coding@gmail.com>
2017-12-15 14:16:27 +0100

    mvp finished

--- server.js
+++ server.js
@@ -1,12 +1,40 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
 
 const app = express();
 const PORT = 3000;
+const dbURL =
+  "mongodb://gecko16:gecko16rulez@ds157158.mlab.com:57158/meeting-planner-mvp";
+var db;
 
-app.listen(PORT, function() {
-  console.log(`Server running on Port ${PORT}`);
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+MongoClient.connect(dbURL, (err, database) => {
+  if (err) return console.log(err);
+  db = database;
+  app.listen(PORT, () => {
+    console.log(`Server running on Port ${PORT}`);
+  });
 });
 
-app.get('/', function(req, res) {
-  res.send('Hello World')
-})
+app.get("/", function(req, res) {
+  db
+    .collection("meetings")
+    .find()
+    .toArray(function(err, result) {
+      if (err) return console.log(err);
+      res.render("index.ejs", { meetings: result });
+    });
+});
+
+app.post("/meetings", (req, res) => {
+  db.collection("meetings").save(req.body, (err, result) => {
+    if (err) return console.log(err);
+
+    console.log("saved to database");
+    res.redirect("/");
+  });
+});
