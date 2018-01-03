b1a4b5efd264289d5c3b5b7466242a51679fae33
toni@tonigibbons.co.uk <toni@tonigibbons.co.uk>
2018-01-03 18:28:56 +0000

    Meeting routes only available if logged in

--- routes/meetings.js
+++ routes/meetings.js
@@ -2,20 +2,34 @@ const express = require('express');
 const router = express.Router();
 const Meeting = require("../models/meeting");
 
-router.get("/create", (req, res) => {
+router.get("/create", isLoggedIn, (req, res) => {
   res.render("create", {title: "GeckoMeet - Create a Meeting"});
 });
 
-router.post("/create", (req, res) => {
+router.post("/create", isLoggedIn, (req, res) => {
   //var title = req.body.title;
   // TODO: save to db, and other clever stuff
   res.redirect("meetings");
 });
 
-router.get("/meetings", (req, res) => {
+router.get("/meetings", isLoggedIn, (req, res) => {
   Meeting.find((err, results) => {
     res.render("meetings", {title: "GeckoMeet - Current Meetings", meetings: results});
   });  
 });
 
 module.exports = router;
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/");
+}
+
+function notLoggedIn(req, res, next) {
+  if (!req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/");
+}
