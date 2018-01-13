e124f3e961bde5a8e5a499c6f6ce4cfb824a4045
toni@tonigibbons.co.uk <toni@tonigibbons.co.uk>
2018-01-13 20:12:28 +0000

    Add passport strategy libs

---
+++ config/passport.js
@@ -0,0 +1,58 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
+const LocalStrategy = require("passport-local").Strategy;
+
+// Use the GoogleStrategy within Passport.
+//   Strategies in Passport require a `verify` function, which accept
+//   credentials (in this case, an accessToken, refreshToken, and Google
+//   profile), and invoke a callback with a user object.
+passport.use(
+  new GoogleStrategy({
+    // options for the google strategy
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL
+  },
+  (accessToken, refreshToken, profile, done) => {
+    // passport callback function
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
+
+// Use the FacebookStrategy with Passport
+passport.use(
+  new FacebookStrategy({
+    clientID: process.env.FACEBOOK_ID,
+    clientSecret: process.env.FACEBOOK_SECRET,
+    callbackURL: process.env.FACEBOOK_CALLBACK_URL
+  },
+  (accessToken, refreshToken, profile, done) => {
+    // passport callback function
+    User.findOrCreate(..., function(err, user) {
+      if (err) { return done(err); }
+      done(null, user);
+    });
+  }
+));
+
+// Use the LocalStrategy with Passport
+passport.use(
+  new LocalStrategy(
+    // options for the local strategy
+    usernameField: "email",
+    passwordField: "password",
+    session: false
+  },
+  (username, password, done) => {
+    // passport callback function
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      if (!user.verifyPassword(password)) { return done(null, false); }
+      return done(null, user);
+    });
+  }
+));
